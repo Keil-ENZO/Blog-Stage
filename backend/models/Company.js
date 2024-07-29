63068ccbbd3653eea2733c79669f9b921884c8e4
Keil-ENZO <enzo.keil06@icloud.com>
2024-07-29 18:31:14 +0200

    feat: Add new models, Company

---
+++ backend/models/Company.js
@@ -0,0 +1,35 @@
+const moogoose = require("mongoose");
+
+const CompanySchema = new moogoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  //   email: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   phone: {
+  //     type: String,
+  //     required: true,
+  //   },
+});
+
+module.exports = moogoose.model("Company", CompanySchema);
