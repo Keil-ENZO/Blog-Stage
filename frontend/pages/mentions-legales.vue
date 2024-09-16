8bb2bbc5a7ecb50e4112d1863de1260e7d48d63f
Keil-ENZO <enzo.keil06@icloud.com>
2024-09-16 16:15:30 +0200

    style: legal

--- frontend/pages/mentions-legales.vue
+++ frontend/pages/mentions-legales.vue
@@ -1,16 +1,20 @@
 <template>
-  <h1>Mentions Légales</h1>
-  <p>
-    <strong>Éditeur du site :</strong> M.KEIL Enzo, Developpeur Web, Grasse,
-    France.
-  </p>
-  <p><strong>Hébergeur :</strong> Hostinger, [Adresse], [Ville].</p>
-  <p>
-    Le site collecte des données personnelles via un formulaire de contact et
-    une inscription à la newsletter. Pour plus d'informations sur l'utilisation
-    de ces données, veuillez consulter notre
-    <a class="link" href="/politique-confidentialite"
-      >Politique de Confidentialité</a
-    >.
-  </p>
+  <div class="py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <h1>Mentions Légales</h1>
+      <p>
+        <strong>Éditeur du site :</strong> M.KEIL Enzo, Developpeur Web, Grasse,
+        France.
+      </p>
+      <p><strong>Hébergeur :</strong> Hostinger.</p>
+      <p>
+        Le site collecte des données personnelles via un formulaire de contact
+        et une inscription à la newsletter. Pour plus d'informations sur
+        l'utilisation de ces données, veuillez consulter notre
+        <a class="link" href="/politique-confidentialite"
+          >Politique de Confidentialité</a
+        >.
+      </p>
+    </div>
+  </div>
 </template>
--- frontend/pages/politique-confidentialite.vue
+++ frontend/pages/politique-confidentialite.vue
@@ -1,63 +1,67 @@
 <template>
-  <h1>Politique de Confidentialité</h1>
-  <p>
-    Cette politique décrit comment nous collectons, utilisons et protégeons vos
-    données personnelles lorsque vous utilisez notre site.
-  </p>
+  <div class="py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <h1>Politique de Confidentialité</h1>
+      <p>
+        Cette politique décrit comment nous collectons, utilisons et protégeons
+        vos données personnelles lorsque vous utilisez notre site.
+      </p>
 
-  <h2>Données collectées</h2>
-  <p>
-    Nous collectons les informations suivantes via notre formulaire de contact
-    et notre newsletter :
-  </p>
-  <ul>
-    <li>
-      <strong>Formulaire de contact :</strong> nom, prénom, adresse email,
-      message.
-    </li>
-    <li><strong>Newsletter :</strong> adresse email.</li>
-  </ul>
+      <h2>Données collectées</h2>
+      <p>
+        Nous collectons les informations suivantes via notre formulaire de
+        contact et notre newsletter :
+      </p>
+      <ul>
+        <li>
+          <strong>Formulaire de contact :</strong> nom, prénom, adresse email,
+          message.
+        </li>
+        <li><strong>Newsletter :</strong> adresse email.</li>
+      </ul>
 
-  <h2>Utilisation des données</h2>
-  <p>Les données collectées sont utilisées aux fins suivantes :</p>
-  <ul>
-    <li>Répondre à vos messages envoyés via le formulaire de contact.</li>
-    <li>
-      Vous envoyer des emails d'actualité et d'information si vous vous êtes
-      inscrit(e) à notre newsletter.
-    </li>
-  </ul>
+      <h2>Utilisation des données</h2>
+      <p>Les données collectées sont utilisées aux fins suivantes :</p>
+      <ul>
+        <li>Répondre à vos messages envoyés via le formulaire de contact.</li>
+        <li>
+          Vous envoyer des emails d'actualité et d'information si vous vous êtes
+          inscrit(e) à notre newsletter.
+        </li>
+      </ul>
 
-  <h2>Conservation des données</h2>
-  <p>
-    Vos données sont conservées uniquement pour la durée nécessaire à la gestion
-    de notre relation avec vous. Vous pouvez à tout moment demander la
-    suppression de vos données personnelles en nous contactant via
-    <a class="link" href="mailto:enzo.keil06@icloud.com"
-      >enzo.keil06@icloud.com</a
-    >.
-  </p>
+      <h2>Conservation des données</h2>
+      <p>
+        Vos données sont conservées uniquement pour la durée nécessaire à la
+        gestion de notre relation avec vous. Vous pouvez à tout moment demander
+        la suppression de vos données personnelles en nous contactant via
+        <a class="link" href="mailto:enzo.keil06@icloud.com"
+          >enzo.keil06@icloud.com</a
+        >.
+      </p>
 
-  <h2>Partage des données</h2>
-  <p>
-    Nous ne partageons pas vos données avec des tiers sans votre consentement,
-    sauf si cela est nécessaire pour des raisons légales.
-  </p>
+      <h2>Partage des données</h2>
+      <p>
+        Nous ne partageons pas vos données avec des tiers sans votre
+        consentement, sauf si cela est nécessaire pour des raisons légales.
+      </p>
 
-  <!-- <h2>Cookies</h2>
+      <!-- <h2>Cookies</h2>
   <p>
     Notre site utilise des cookies pour analyser le trafic via des services
     tiers comme Google Analytics. Vous pouvez désactiver les cookies dans les
     paramètres de votre navigateur.
   </p> -->
 
-  <h2>Vos droits</h2>
-  <p>
-    Conformément au Règlement Général sur la Protection des Données (RGPD), vous
-    avez le droit d'accéder à vos données, de les modifier ou de les supprimer.
-    Vous pouvez exercer ces droits en nous contactant à
-    <a class="link" href="mailto:enzo.keil06@icloud.com"
-      >enzo.keil06@icloud.com</a
-    >.
-  </p>
+      <h2>Vos droits</h2>
+      <p>
+        Conformément au Règlement Général sur la Protection des Données (RGPD),
+        vous avez le droit d'accéder à vos données, de les modifier ou de les
+        supprimer. Vous pouvez exercer ces droits en nous contactant à
+        <a class="link" href="mailto:enzo.keil06@icloud.com"
+          >enzo.keil06@icloud.com</a
+        >.
+      </p>
+    </div>
+  </div>
 </template>
